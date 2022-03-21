27c5a769202e623897da33d022f2eba42cfb6f1c
unknown <abegundeosamuel@gmail.com>
2022-03-21 13:49:25 +0100

    Testimonials view

---
+++ src/Component/Testimonials/Testimonials.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Testimonials.css'
+import Food from '../../Assets/food5.png'
+import { AiOutlineArrowLeft, BsArrowRight } from 'react-icons/all'
+export default function Testimonials() {
+    return (
+        <div className='testimonials_Container-parent'>
+            <div className='testimonials_container'>
+                <div className='testimonials-text'>
+                    <div className='testimonials-text-header'>
+                        Testimonials
+                    </div>
+                    <div className='testimonials-text-desc'>
+                        <p>What our clients says</p>
+                        <p>
+                            "Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu
+                            fugiat nulla pariatur. <br />
+                            Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu
+                            fugiat nulla pariatur.
+                            "
+                        </p>
+                    </div>
+                </div>
+                <div className='testimonials-image'>
+                    <img src={Food} alt='food5' />
+                </div>
+            </div>
+            <div className='testimonials-name-container'>
+                <div className='testimonials-name'>
+                    Jane doe
+                </div>
+                <div className='testimonials-symbols'>
+                    <AiOutlineArrowLeft size={20} color='#707070'/>
+                    <BsArrowRight color='#e3b32d' size={20}/>
+                </div>
+            </div>
+        </div>
+    )
+}
